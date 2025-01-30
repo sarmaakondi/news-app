511b055126dfa668805341396372bcbdf80352be
sarmaakondi <avnmsarma@gmail.com>
2025-01-30 18:36:50 +1100

    feat: implement searchbar functionality with added debounce for smooth API calls

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+import debounce from "lodash.debounce";
+import axios from "axios";
+
 const SearchBar = ({ onSearch }) => {
-    return <input type="text" placeholder="Search articles..." />;
+    const [query, setQuery] = useState("");
+
+    const debouncedSearch = debounce(async (searchTerm) => {
+        console.log(process.env.REACT_APP_API_URL);
+        try {
+            const response = await axios.get(process.env.REACT_APP_API_URL, {
+                params: {
+                    q: searchTerm,
+                },
+            });
+            onSearch(response.data);
+        } catch (error) {
+            console.error("Search error:", error);
+            onSearch([], error);
+        }
+    }, 500);
+
+    useEffect(() => {
+        if (query) {
+            debouncedSearch(query);
+        }
+        return () => debouncedSearch.cancel();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [query]);
+
+    return (
+        <input
+            type="text"
+            placeholder="Search articles..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+        />
+    );
 };
 
 export default SearchBar;
